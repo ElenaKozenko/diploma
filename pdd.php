1810a1f697492d3e7f12ecf54bff7530eb615c07
ElenaKozenko <elenakozenko12@gmail.com>
2022-03-24 20:10:10 +0300

    обновление

---
+++ pdd.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8" />
+  <title>Главная</title>
+  </head>
+<body>
+<div>
+<a href = 'pdd.php' >Тренажер</a>
+<a href = 'pdd.php' >Экзамен</a>
+<a href = 'newticket.php' >Создание билета</a>
+<a href = 'pdd.php' >Смена пароля</a>
+</div>
+</body>
+</html>
